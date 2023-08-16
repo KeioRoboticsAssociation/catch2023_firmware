c53490a088bff5b10a9bbbfc0ca69b5175599f46
hirobon1690 <hirobon1690@gmail.com>
2023-08-16 20:17:54 +0900

    UPDATE: 根本を回すところまで

--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-
-int main(void) {
-    stdio_init_all();
-    printf("Hello, world!\n");
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <pico/stdlib.h>
+#include <stdio.h>
+#include "amt232.h"
+#include "encoderBase.h"
+#include "gpio.h"
+#include "motor.h"
+#include "pwm.h"
+#include "qenc.h"
+
+const uint CS_PIN = 25;
+AMT232 amt232(CS_PIN, 3, 0, 2);
+Qenc enc(12);
+Gpio cs0(25, OUTPUT);
+Gpio dum0(1, OUTPUT);
+Gpio dum1(6, OUTPUT);
+Pwm pwm0(19, 10000, 1);
+Pwm pwm1(16);
+Gpio dir0(20, OUTPUT);
+Gpio dir1(17, OUTPUT);
+Motor motor[2] = {
+    Motor(dir0, pwm0, amt232, dum0),
+    Motor(dir1, pwm1, enc, dum1)};
+
+bool timer_cb(repeating_timer_t* rt) {
+    motor[0].timer_cb();
+    // motor[1].timer_cb();
+    return true;
+}
+
+bool timer_cb_pos(repeating_timer_t* rt) {
+    motor[0].timer_cb_pos();
+    // motor[1].timer_cb_pos();
+    return true;
+}
+
+void initTimer() {
+    static repeating_timer_t timer;
+    static repeating_timer_t timer1;
+    static repeating_timer_t timer2;
+    add_repeating_timer_ms(-10, timer_cb, NULL, &timer);
+    add_repeating_timer_ms(-100, timer_cb_pos, NULL, &timer1);
+}
+
+int main() {
+    stdio_init_all();
+
+    sleep_ms(10);
+    cs0.init();
+    dum0.init();
+    dum1.init();
+    cs0.write(1);
+    dum0.write(1);
+    dum1.write(1);
+    motor[0].init();
+    motor[0].setPosGain(3.8, 0.12, 1.2);
+    // motor[0].disablePosPid();
+    sleep_ms(10);
+    motor[1].init();
+    // motor[0].duty(0.1);
+    // motor[1].duty(0.1);
+    sleep_ms(10);
+    amt232.init();
+    initTimer();
+    while (1) {
+        // motor[0].setVel(90);
+        motor[0].setPos(90);
+        // tx_data[0] = 0x0c;
+        // tx_data[1] = 0x00;
+
+        // dum0.write(0);
+        // spi_write_read_blocking(spi0, tx_data, rx_data, 2);
+        // dum0.write(1);
+        // printf("data: %x, %x\n", rx_data[0], rx_data[1]);
+        // printf("%d, %d, %d\n",amt232.get(),amt232.getRaw(),amt232.get()%4096);
+        sleep_ms(6000);
+        motor[0].setPos(0);
+        sleep_ms(6000);
+    }
+}
